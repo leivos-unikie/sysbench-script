d4ec684f9feac9b970cfcfcfbc483d490fa63cf8
Samuli Leivo <samuli.leivo@unikie.com>
2024-02-23 12:18:24 +0200

    Add script version for plotting NUC results.
    Signed-off-by: Samuli Leivo <samuli.leivo@unikie.com>

---
+++ visualize_results_nuc.py
@@ -0,0 +1,146 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+from extract_value import *
+
+
+path_to_data = "./../sysbench_result_data/nuc/"
+
+# Limit the range of plotted test runs
+first_test_run_no = 0   # test runs with lower number won't be shown
+last_test_run_no = 3   # test runs with higher number won't be shown
+
+
+test_type = None
+test = None
+
+# Select which test type to plot: 0-5
+def select_test_type():
+
+    global test_type
+
+    if test == 0:
+        test_type = "memory_rd_1thread_report"
+    elif test == 1:
+        test_type = "memory_wr_1thread_report"
+    elif test == 2:
+        test_type = "memory_rd_report"
+    elif test == 3:
+        test_type = "memory_wr_report"
+    elif test == 4:
+        test_type = "cpu_1thread_report"
+    elif test == 5:
+        test_type = "cpu_report"
+    else:
+        exit(1)
+
+    print(test)
+    print(test_type)
+    print()
+
+
+def list_vm_files(file_list, vm_name):
+    list_vm = []
+    for f in file_list:
+        if f.find(vm_name) != -1 and f.split('/')[-1].find('report') != -1:
+            list_vm.append(f)
+    return list_vm
+
+
+def list_files(path):
+    file_list = []
+    for path, subdirs, files in os.walk(path):
+        for name in files:
+            # print(os.path.join(path, name))
+            file_list.append(os.path.join(path, name))
+    file_list.sort(key=os.path.getctime)
+    return file_list
+
+
+def extract_values(file_list, report_type, detect_string, start_string, end_string):
+
+    values = [0] * (last_test_run_no + 1)
+
+    for f in file_list:
+        if f.endswith(report_type):
+
+            # Print also some file info.
+            # time_created = os.path.getctime(f)
+            # time_created_h = datetime.datetime.fromtimestamp(time_created)
+            # print(time_created_h)
+            # print(f)
+            # print()
+
+            index = int(f.split('/')[-2].split('_')[0])
+            if index > last_test_run_no: continue
+
+            value = float(extract(f, detect_string, start_string, end_string))
+
+            values[index] = value
+
+    values_sliced = values[first_test_run_no:]
+
+    return values_sliced
+
+
+def vm_bar(plt, file_list, vm_name, x_offset, c):
+    vm_files = list_vm_files(file_list, vm_name)
+
+    if test < 4:
+        # For memory test results
+        vm_values = extract_values(vm_files, test_type, "MiB transferred", "(", " MiB/sec")
+    elif test > 3:
+        # For cpu test results
+        vm_values = extract_values(vm_files, test_type, "events per second:", "events per second:", "\n")
+    else:
+        return
+
+    print("List of " + vm_name + " results")
+    print(vm_values)
+    print()
+
+    # Here rounding caused errors if 'last_test_run_no + x_offset' was used as stop.
+    xpoints = list(np.arange(first_test_run_no + x_offset, last_test_run_no + x_offset + 0.9, 1))
+    plt.bar(xpoints, vm_values, label=vm_name, color=c, width=0.1)
+    return
+
+def main_loop(plt):
+
+    files_list = list_files(path_to_data)
+
+    vm_bar(plt, files_list, "host", 0, 'b')
+    vm_bar(plt, files_list, "net-vm", 0.1, 'g')
+    # vm_bar(plt, files_list, "gui-vm", 0.2, 'r')
+    # vm_bar(plt, files_list, "chromium-vm", 0.3, 'y')
+    # vm_bar(plt, files_list, "gala-vm", 0.4, 'k')
+    # vm_bar(plt, files_list, "zathura-vm", 0.5, 'm')
+    # vm_bar(plt, files_list, "ids-vm", 0.6, 'lime')
+    # vm_bar(plt, files_list, "audio-vm", 0.7, 'pink')
+    # vm_bar(plt, files_list, "element-vm", 0.8, 'turquoise')
+
+    plt.legend(loc='lower left')
+
+    if test < 4:
+        # For cpu test
+        plt.ylabel('Events per second')
+    else:
+        # For memory test
+        plt.ylabel('MiB/sec')
+
+    plt.title(test_type)
+    plt.xlabel('Test run')
+
+    plt.savefig("{}{}_from_test_runs_{}-{}.jpg".format(path_to_data, test_type, first_test_run_no, last_test_run_no))
+
+    # Print the chart
+    # plt.show()
+
+
+plt.xticks(np.arange(first_test_run_no, last_test_run_no + 1, step=1))
+
+for t in range(0, 6):
+    test = t
+    select_test_type()
+    main_loop(plt)
+    plt.cla()
